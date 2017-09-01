848c1b6a28b718183ab7d18c9de22e037546d6e4
samueldibella <samuel.dibella@gmail.com>
2017-08-31 20:37:22 -0400

    Use isbnlib to import metadata and export into formatted .csv

--- isbn-to-metadata/isbn-metadata.py
+++ isbn-to-metadata/isbn-metadata.py
@@ -1,10 +1,19 @@
 import isbnlib
 
-# isbnlib.registry.bibformatters = "json"
 inputFile = "input.txt"
 outputFile = open('output.csv', 'a')
 f = open(inputFile)
+
+metadata = {}
+
 with f:
     for line in f:
-        outputFile.write(str(isbnlib.meta(line)))
+        # metadata is default read as JSON object & implicitly cast to dict
+        metadata = isbnlib.meta(line)
+
+        outputFile.write(metadata['Title'] + ", ")
+        # Authors returns as a list, so string cast and separator needed
+        outputFile.write("; ".join(metadata['Authors']) + ", ")
+        outputFile.write(metadata['Year'] + "")
+
         outputFile.write("\n")
